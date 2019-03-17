fc1feda0afe5c36453773d6d551efcecf7ecc975
vpaulg <vpaulg@gmail.com>
2019-03-16 21:36:25 -0500

    contact bugfix1

--- contactform/contactform.php
+++ contactform/contactform.php
@@ -17,7 +17,6 @@
         }
 
         // Set the recipient email address.
-        // FIXME: Update this to your desired email address.
         $recipient = "vpaulg@gmail.com";
 
         // Set the email subject.
@@ -35,7 +34,7 @@
         if (mail($recipient, $subject, $email_content, $email_headers)) {
             // Set a 200 (okay) response code.
             http_response_code(200);
-            echo "Thank You! Your message has been sent.";
+            echo "Thank You! Your message from $email_headers has been sent.";
         } else {
             // Set a 500 (internal server error) response code.
             http_response_code(500);
